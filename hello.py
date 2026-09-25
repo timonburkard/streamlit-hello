9491656e1e36408130d098e7e23688a3bab23285
Timon Burkard <timon.burkard@gwf.ch>
2026-09-25 14:27:48 +0200

    make it nicer

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write("Hello World")
+st.set_page_config(page_title="Hello App", page_icon="👋")
+
+st.title("Hello, world! 👋")
+st.caption("Welcome to your Streamlit app.")
+st.write("This page is a little cleaner and friendlier.")
+
+if st.button("Click me"):
+    st.success("Nice to meet you!")
