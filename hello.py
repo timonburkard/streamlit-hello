e46c4fc14b4acff84fdf32261d20c12e6aa72d7e
Timon Burkard <timon.burkard@gwf.ch>
2026-09-25 14:29:08 +0200

    one more change

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="Hello App", page_icon="👋")
 
 st.title("Hello, world! 👋")
-st.caption("Welcome to your Streamlit app.")
-st.write("This page is a little cleaner and friendlier.")
+st.caption("Welcome to my Streamlit app.")
+st.write("It is just a simple test.")
 
 if st.button("Click me"):
     st.success("Nice to meet you!")
